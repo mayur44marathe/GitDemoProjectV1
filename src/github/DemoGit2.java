69ab028896e12842e03241b4188bc7818aa2c18f
mayur44marathe <125548845+mayur44marathe@users.noreply.github.com>
2023-02-20 15:32:19 +0530

    Update DemoGit2.java
    some changes has been done from my side.
    please check and review the code.

--- src/github/DemoGit2.java
+++ src/github/DemoGit2.java
@@ -4,7 +4,9 @@ public class DemoGit2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Country");
+		//System.out.println("Hello Country");
+		
+		System.out.println("Hello New Country");
 
 	}
 
